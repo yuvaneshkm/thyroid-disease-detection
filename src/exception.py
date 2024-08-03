76ffbe8d3a442e2f212d24c60bd7c9733134b102
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-08-04 01:12:14 +0530

    exception-file-updated

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,29 @@
 # Importing necessary libraries:
-import os
 import sys
 import warnings
+
 warnings.filterwarnings("ignore")
 
+
+# Creating a class to handle custom exception:
+class CustomException(Exception):
+
+    def __init__(self, error_message: Exception):
+
+        _, _, exception_traceback = sys.exc_info()
+
+        filename = exception_traceback.tb_frame.f_code.co_filename
+        linenumber = exception_traceback.tb_lineno
+        errormessage = str(error_message)
+
+        self.messsage = f"Error occured in Python script name: [{filename}], line number: [{linenumber}] and error message: [{errormessage}]"
+
+    def __str__(self) -> str:
+        return self.messsage
+
+
+if __name__ == "__main__":
+    try:
+        9 / 0
+    except Exception as ex:
+        raise CustomException(ex)
